7a25404060ffa39206a8e1a7f88f035139921ce3
lcw3176 <lcw3176@naver.com>
2023-03-09 16:43:18 +0900

    first commit

---
+++ src/main/java/com/joebrooks/community/MapShotCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.joebrooks.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapShotCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapShotCommunityApplication.class, args);
+    }
+
+}
